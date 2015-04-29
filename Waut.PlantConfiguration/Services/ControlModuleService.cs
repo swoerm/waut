75f21589d8a18e08d263ae0868817e56ae57419a
swoerm <sven.woermann@gmail.com>
2015-04-29 12:17:35 +0200

    dgdg

---
+++ Waut.PlantConfiguration/Services/ControlModuleService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waut.PlantConfiguration.Services
+{
+    class ControlModuleService
+    {
+        public ControlModuleService()
+        {
+
+        }
+
+
+    }
+}
