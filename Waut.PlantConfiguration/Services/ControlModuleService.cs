a6cfc00ec6ca9c4ea756aa530864d049e69faa8e
Stefannel <stefancnel@outlook.com>
2015-04-29 15:09:22 +0200

    hewqq

--- Waut.PlantConfiguration/Services/ControlModuleService.cs
+++ Waut.PlantConfiguration/Services/ControlModuleService.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Waut.PlantConfiguration.Models;
+//Excel
+using System.Xml.Serialization;
+using System.IO;
+using System.Data;
+using System.Diagnostics;
+using System.Data.OleDb;
+//Excel
+
 
 namespace Waut.PlantConfiguration.Services
 {
@@ -24,6 +32,7 @@ namespace Waut.PlantConfiguration.Services
 
             ControlModule cm = new ControlModule();
             cm.Description = "af as asd as ";
+            ExcelRead();
 
 
 
@@ -33,6 +42,30 @@ namespace Waut.PlantConfiguration.Services
 
 
             return list;
+        }
+
+        public static void/*string[]*/ ExcelRead()
+        {
+            string[] arr = { "wort inlet holding vessel", "121", "Type", "Unit", "Format", "Krones", "Project", "1", "2", "3", "4" };
+
+            string con = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\snel\Source\Repos\NewRepo\Waut.PlantConfiguration\Data\SAB_ONITSHSA_IO_List_BH_PLC1_REV14.xls;Extended Properties='Excel 8.0;HDR=Yes;'";
+            using (OleDbConnection connection = new OleDbConnection(con))
+            {
+                connection.Open();
+                OleDbCommand command = new OleDbCommand("select * from [BH1PB01$]", connection);
+                using (OleDbDataReader dr = command.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        var row1Col0 = dr[0];
+                        var row1Col1 = dr[1];
+                        Console.WriteLine(row1Col0);
+                        Console.WriteLine(row1Col1);
+
+                    }
+                }
+            }
+            // return arr;
         }
     }
 }
