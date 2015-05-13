12809824bf4bbef9f2dbed492e203a3eeffee217
swoerm <sven.woermann@gmail.com>
2015-05-13 10:19:27 +0200

    fgdfgdf

---
+++ Configurator/Shell.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel.Composition;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Waut.Configurator
+{
+    /// <summary>
+    /// Interaction logic for Shell.xaml
+    /// </summary>
+    [Export]
+    public partial class Shell : Window
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Sets the ViewModel.
+        /// </summary>
+        /// <remarks>
+        /// This set-only property is annotated with the <see cref="ImportAttribute"/> so it is injected by MEF with
+        /// the appropriate view model.
+        /// </remarks>
+        [Import]
+        [SuppressMessage("Microsoft.Design", "CA1044:PropertiesShouldNotBeWriteOnly", Justification = "Needs to be a property to be composed by MEF")]
+        ShellViewModel ViewModel
+        {
+            set
+            {
+                this.DataContext = value;
+            }
+        } 
+    }
+}
